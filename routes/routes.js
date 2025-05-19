ad876b5f62907360bcb8df1fafcbb9f2f68e3184
Enrique <TorresCervantesLE@gmail.com>
2025-05-19 10:42:07 -0600

    Update routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -64,26 +64,36 @@ router.patch('/tasks/:id', async (req, res) => {
     const objectId = new ObjectId(req.params.id);
     const data = req.body;
 
+    // No eliminar los valores falsy si son booleanos
     Object.keys(data).forEach(key => {
-      // Si el valor es falsy (null o undefined), eliminar la propiedad
+      // Si el valor es falsy y la propiedad no es un booleano, eliminarla
       if (data[key] === undefined || data[key] === null) {
         delete data[key];
       }
     });
 
+    // Hacer la actualización en la base de datos
     const task = await Task.updateOne(
-      {_id: objectId}, {$set: data}
-    )
+      { _id: objectId },
+      { $set: data }
+    );
+
+    // Revisamos si la tarea fue actualizada correctamente
+    if (task.modifiedCount === 0) {
+      return res.status(404).send(json_struct({
+        message: "Task not found or no changes made"
+      }));
+    }
 
     return res.send(json_struct({
       message: "Modified!",
-      data: task
+      data: data // Enviar los datos actualizados
     }));
     
   } catch (error) {
     return res.status(500).send(`[usr_mod] Something wrong at:  ${error}`);
   }
-
 });
+
 
 export default router;
